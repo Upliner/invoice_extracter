87391040edad07408a2fd0a5d89348940c866b6e
Michael Vigovsky <upliner@gmail.com>
2015-06-02 21:36:06 +0300

    More advanced totals and VAT parsing, including OCR

--- invoice_extracter.py
+++ invoice_extracter.py
@@ -25,6 +25,8 @@ u"БИК": "044525201",
 u"Корсчет": "30101810000000000201", 
 }
 
+drp = re.UNICODE | re.IGNORECASE # default regexp parameters
+
 # Алгоритм работы:
 # В первом проходе ищем надписи ИНН, КПП, БИК и переносим числа, следующие за ними в
 # соответствующие поля. 
@@ -39,14 +41,37 @@ class InvParseException(Exception):
 
 # Заполняет поле с именем fld и проверяет, чтобы в нём уже не присутствовало другое значение
 def fillField(pr, fld, value):
+    if value == None: return
     ourVal = our.get(fld)
-    if value == None or (fld == u"ИНН" and value == ourVal): return
     oldVal = pr.get(fld)
     if value == ourVal and oldVal != ourVal: return
     if oldVal != None and oldVal != value and value != ourVal and oldVal != ourVal:
        if fld == u"Счет": return
        raise InvParseException(u"Найдено несколько различных %s: %s и %s" % (fld, oldVal, value))
     pr[fld] = value
+
+def fillTotal(pr, val):
+    if val == None: return
+    oldTotal = pr.get(u"Итого")
+    if (val == pr.get(u"ИтогоБезНДС")): return
+    if (val == pr.get(u"ИтогоСНДС")): return
+    if oldTotal != None:
+        if oldTotal != val:
+            fillField(pr, u"ИтогоБезНДС", min(val, oldTotal))
+            fillField(pr, u"ИтогоСНДС", max(val, oldTotal))
+            del pr[u"Итого"]
+    else:
+        pr[u"Итого"] = val
+
+def fillVatType(pr, content):
+    if content == u"Без НДС" or content == u"Без налога (НДС)":
+        fillField(u"СтавкаНДС", u"БезНДС")
+        fillField(u"СуммаНДС", 0)
+    if "18%" in content: fillField(pr, u"СтавкаНДС", u"18%")
+    if "10%" in content: fillField(pr, u"СтавкаНДС", u"10%")
+    if "0%"  in content:
+        fillField(pr, u"СтавкаНДС", u"0%") 
+        fillField(pr, u"СуммаНДС", 0)
 
 # Проверка полей
 def checkInn(val):
@@ -61,7 +86,7 @@ def checkBic(val):
 
 # Убрать лишние данные из номера счёта
 def stripInvoiceNumber(num):
-    m = re.search(ur"\bот:? *[0-3]?\d(\.[0-1]\d\.| *[а-яА-Я]* )(20)?\d\d( *г([г\.]|ода?)?)?", num, re.UNICODE)
+    m = re.search(ur"\bот:? *[0-3]?\d(\.[0-1]\d\.| *[а-яА-Я]* )(20)?\d\d( *г([г\.]|ода?)?)?", num, drp)
     if m: return num[0:m.end()]
     return num
 
@@ -109,7 +134,10 @@ def processPdfLine(pdf, pl, pr):
     processCellContent(content, getValueToTheRight, pl, pr)
 
 def parse(num):
-    return float(re.sub(ur"руб\.?| ","", num).replace(",",".").strip())
+    try:
+       return float(re.sub(ur"руб\.?| ","", num).replace(",",".").strip())
+    except ValueError:
+       return None
 
 def processCellContent(content, getValueToTheRight, firstCell, pr):
     def getSecondValue(separator = None):
@@ -123,7 +151,7 @@ def processCellContent(content, getValueToTheRight, firstCell, pr):
         return getValueToTheRight(firstCell)[0]
 
     for fld, check in [[u"ИНН", checkInn], [u"КПП", checkKpp], [u"БИК", checkBic]]:
-        if re.match(u"[^a-zA-Zа-яА-Я]?"  + fld + u"\\b", content, re.UNICODE | re.IGNORECASE):
+        if re.match(u"[^a-zA-Zа-яА-Я]?"  + fld + u"\\b", content, drp):
             val = getSecondValue()
             if val == None: return
             rm = re.match("[0-9]+", val)
@@ -133,7 +161,7 @@ def processCellContent(content, getValueToTheRight, firstCell, pr):
             check(val)
             fillField(pr, fld, val)
             return
-    if re.match(u"Сч[её]т *(на оплату|№|.*? от [0-9][0-9]?[\\. ])", content, re.UNICODE | re.IGNORECASE):
+    if re.match(u"Сч[её]т *(на оплату|№|.*? от [0-9][0-9]?[\\. ])", content, drp):
         text = content
         val, cell = getValueToTheRight(firstCell)
         while val != None:
@@ -142,12 +170,12 @@ def processCellContent(content, getValueToTheRight, firstCell, pr):
             val, cell = getValueToTheRight(cell)
         fillField(pr, u"Счет", stripInvoiceNumber(text.strip().replace("\n"," ").replace("  "," ")))
         return
-    if re.match(u"ИНН */ *КПП\\b", content, re.UNICODE | re.IGNORECASE):
+    if re.match(u"ИНН */ *КПП\\b", content, drp):
         val = getSecondValue()
         if val == None: return
-        rm = re.match("([0-9]{10}) */? *([0-9]{9})\\b", val, re.UNICODE)
+        rm = re.match("([0-9]{10}) */? *([0-9]{9})\\b", val, drp)
         if rm:
-            rm = re.match("([0-9]{12}) */?\\b", val, re.UNICODE)
+            rm = re.match("([0-9]{12}) */?\\b", val, drp)
             if rm == None: return
             checkInn(rm.group(1))
             fillField(pr, u"ИНН", rm.group(1))
@@ -156,51 +184,25 @@ def processCellContent(content, getValueToTheRight, firstCell, pr):
         checkKpp(rm.group(2))
         fillField(pr, u"КПП", rm.group(2))
         return
-    if (re.match(u"Итого|Всего", content, re.UNICODE | re.IGNORECASE) and
-           (u"без НДС" in content or not u"НДС" in content)):
-        if ":" in content:
-            val = getSecondValue(":")
-        else:
-            val = getValueToTheRight(firstCell)[0]
+    if (re.match(u"Итого|Всего", content, drp) and
+            (re.search(u"(с|без) *НДС", content, drp) or not u"НДС" in content)):
+        if ":" in content: val = getSecondValue(":")
+        else: val = getValueToTheRight(firstCell)[0]
         if val == None: return
-        try:
-            val = parse(val)
-        except ValueError:
-            return
-        oldTotal = pr.get(u"Итого")
-        if (val == pr.get(u"ИтогоБезНДС")): return
-        if (val == pr.get(u"ИтогоСНДС")): return
-        if oldTotal != None:
-            if oldTotal != val:
-                pr[u"ИтогоБезНДС"] = min(val, oldTotal)
-                pr[u"ИтогоСНДС"] = max(val, oldTotal)
-                del pr[u"Итого"]
-        else:
-            pr[u"Итого"] = val
+        fillTotal(pr, parse(val))
         return
     if u"НДС" in content:
-        if content == u"Без НДС" or content == u"Без налога (НДС)":
-            pr[u"СтавкаНДС"] = u"БезНДС"
-            pr[u"СуммаНДС"] = 0
-        if "18%" in content: pr[u"СтавкаНДС"] = u"18%"
-        if "10%" in content: pr[u"СтавкаНДС"] = u"10%"
-        if "0%"  in content: pr[u"СтавкаНДС"] = u"0%"; pr[u"СуммаНДС"] = 0
-        if re.match(ur"(Всего|Итого|Сумма|в т\.ч\.|в том числе).*?", content, re.UNICODE | re.IGNORECASE):
-            if ":" in content:
-                val = getSecondValue(":")
-            else:
-                val = getValueToTheRight(firstCell)[0]
-            if (re.match(ur"Без", content, re.IGNORECASE)):
-                pr[u"СтавкаНДС"] = u"БезНДС"
-                pr[u"СуммаНДС"] = 0
+        fillVatType(pr, content)
+        if re.match(ur"(Всего|Итого|Сумма|Включая|в т\.ч\.|в том числе).*?", content, drp):
+            if ":" in content: val = getSecondValue(":")
+            else: val = getValueToTheRight(firstCell)[0]
+            if (re.match(ur"Без", val, drp)):
+                fillField(pr, u"СтавкаНДС", u"БезНДС")
+                fillField(pr, u"СуммаНДС", 0)
                 return 
-            try:
-                val = parse(val)
-            except ValueError:
-                return
-            pr[u"СуммаНДС"] = val
+            fillField(pr, u"СуммаНДС", parse(val))
             return
-        
+    
     findBankAccounts(content, pr)
 
 def findBankAccounts(text, pr):
@@ -219,7 +221,7 @@ def findBankAccounts(text, pr):
     
     if not u"р/с" in pr or hasIncomplete:
         # В некоторых документах р/с написан с пробелами
-        for w in re.finditer(u"[0-9]{4} *[0-9]810 *[0-9]{4} *[0-9]{4} *[0-9]{4}\\b", text, re.UNICODE):
+        for w in re.finditer(u"[0-9]{4} *[0-9]810 *[0-9]{4} *[0-9]{4} *[0-9]{4}\\b", text, drp):
             processAcc(w.group(0).replace(" ", ""))
 
 nap = u"[^a-zA-Zа-яА-Я]?" # Non-alpha prefix
@@ -232,15 +234,15 @@ def processText(text, pr):
             [u"ИНН", u"[0-9О]{10}|[0-9О]{12}"],
             [u"КПП", u"[0-9О]{9}"],
             [u"БИК", u"[0О]4[0-9О]{7}"]]:
-        for val in re.finditer(nap + fld + u"\\n? *(%s)\\b" % regexp, text, re.UNICODE | re.IGNORECASE):
+        for val in re.finditer(nap + fld + u"\\n? *(%s)\\b" % regexp, text, drp):
             fillField(pr, fld, val.group(1).replace(u"О", "0"))
 
-    rr = re.search(u"^ *Сч[её]т *(на оплату|№|.*?\\bот *[0-9]).*", text, re.UNICODE | re.IGNORECASE | re.MULTILINE)
+    rr = re.search(u"^ *Сч[её]т *(на оплату|№|.*?\\bот *[0-9]).*", text, drp | re.MULTILINE)
     if rr: fillField(pr, u"Счет", stripInvoiceNumber(rr.group(0)))
 
     # Поиск находящихся рядом пар ИНН/КПП с совпадающими первыми четырьмя цифрами
     if u"ИНН" not in pr and u"КПП" not in pr:
-        results = re.findall(u"([0-9О]{10}) *[\\\\\\[\\]\\|/ ] *([0-9О]{9})\\b", text, re.UNICODE)
+        results = re.findall(ur"([0-9О]{10}) *[\\\[\]\|/ ] *([0-9О]{9})\b", text, drp)
         results = [[v.replace(u"О", "0") for v in r] for r in results]
         for inn, kpp in results:
             if inn[0:4] == kpp[0:4]:
@@ -256,18 +258,28 @@ def processText(text, pr):
     # Если предыдущие шаги не дали никаких результатов, вставляем как ИНН, КПП и БИК
     # первые подходящие цифры
     if u"ИНН" not in pr:
-        rm = re.search(nap + u"\\b([0-9О]{10}|[0-9О]{12})\\b" + bndry, text, re.UNICODE)
+        rm = re.search(nap + u"\\b([0-9О]{10}|[0-9О]{12})\\b" + bndry, text, drp)
         if rm: fillField(pr, u"ИНН", rm.group(1).replace(u"О", "0"))
     # Ищем КПП только если ИНН десятизначный
     if u"КПП" not in pr and (u"ИНН" not in pr or len(pr[u"ИНН"]) == 10):
-        rm = re.search(u"\\b([0-9О]{9})" + bndry, text, re.UNICODE)
+        rm = re.search(u"\\b([0-9О]{9})" + bndry, text, drp)
         if rm: fillField(pr, u"КПП", rm.group(1).replace(u"О", "0"))
     if u"БИК" not in pr:
-        rm = re.search(u"\\b([0О]4[0-9О]{7})\\b" + bndry, text, re.UNICODE)
+        rm = re.search(u"\\b([0О]4[0-9О]{7})\\b" + bndry, text, drp)
         if rm: fillField(pr, u"БИК", rm.group(1).replace(u"О", "0"))
 
+    for r in re.finditer(ur"Итого( [а-яА-Я ]*)?:?\n? *([0-9\., ]*)", text, drp):
+        if r.group(1) == None or (re.match(u"(c|без) *НДС",r.group(1), drp) or not u"НДС" in r.group(1)):
+            fillTotal(pr, parse(r.group(2).strip(".,")))
+    for r in re.finditer(ur"(?:Всего |Итого |Сумма |в т\.ч\.|в том числе |включая ) *" +
+            ur"НДС *(\([0-9%]*)?(?: [а-яА-Я \)]*)?\.?:?\n? *([0-9\., ]*)", text, drp):
+        if r.group(1) != None: fillVatType(pr, r.group(1))
+        fillField(pr, u"СуммаНДС", parse(r.group(2).strip(".,")))
+    if re.search(ur"Без *(налога)? *\(?НДС", text, drp):
+        fillField(pr, u"СтавкаНДС", u"БезНДС")
+
 def processImage(image, pr):
-    debug = True
+    debug = False
     text = image_to_string(image, lang="rus").decode("utf-8")
     if debug:
         with open("invext-debug.txt","w") as f:
